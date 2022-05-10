d87bbe6e0ba75ed9b401bc5163261f81e662080e
Troy McCall <troyxmccall@gmail.com>
2022-05-10 19:54:28 -0400

    update build

---
+++ cmd/janet/janet-go-builder.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#From https://golang.org/doc/install/source#environment
+platforms=(linux)
+arches=(386 amd64 arm arm64 mips mipsle mips64 mips64le ppc64 ppc64le s390x wasm)
+
+#.go file to build
+test "$1" && target="$1"
+
+if ! test "$target"; then
+    target="main.go"
+fi
+
+binaryName="$2"
+
+for platform in "${platforms[@]}"; do
+    for arch in "${arches[@]}"; do
+        goos=${platform}
+        goarch=${arch}
+
+        output="$binaryName"
+        output="$(basename $target | sed 's/\.go//')"
+
+        [[ "windows" == "$goos" ]] && output="$output.exe"
+
+        destination="$(dirname $target)/builds/production/$goos/$goarch/$output"
+
+        echo -e "\e[00;33mGOOS=$goos GOARCH=$goarch go build -ldflags \"-w\" -o $destination $target\e[00m"
+        GOOS=$goos GOARCH=$goarch go build -ldflags "-w" -o "$destination" "$target"
+    done
+done

dc879049312c5b4bf01ac691273b2a84ff26c5ff
Troy McCall <troyxmccall@gmail.com>
2022-05-10 19:28:29 -0400

    no privledge

--- goreleaser.sh
+++ goreleaser.sh
@@ -3,7 +3,7 @@
 cd "${0%/*}" || exit
 
 docker run \
-	--rm -ti --privileged \
+	--rm -ti \
 	-v $(pwd):/go/src/github.com/troyxmccall/janet \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v $HOME/.config/goreleaser/github_token:/root/.config/goreleaser/github_token \
